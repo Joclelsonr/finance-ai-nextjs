83c248b65184f9a0ae8d6b24a3e2da865829da07
Joclelson Rodrigues <jocleelson.159@hotmail.com>
2024-11-05 17:53:16 -0300

    feat: :sparkles: add authentication with clerk

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,17 @@
 import Image from "next/image";
+import { Button } from "../components/ui/button";
+import { LogInIcon } from "lucide-react";
+import { SignInButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 const LoginPage = () => {
+  const { userId } = auth();
+
+  if (userId) {
+    redirect("/");
+  }
+
   return (
     <div className="grid h-full grid-cols-2">
       {/* ESQUERDA */}
@@ -18,6 +29,12 @@ const LoginPage = () => {
           monitorar suas movimentações, e oferecer insights personalizados,
           facilitando o controle do seu orçamento.
         </p>
+        <SignInButton>
+          <Button variant="outline">
+            <LogInIcon className="mr-2" />
+            Fazer login ou criar conta
+          </Button>
+        </SignInButton>
       </div>
       {/* DIREITA */}
       <div className="relative h-full w-full">
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
-import { Button } from "./components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 export default function Home() {
+  const { userId } = auth();
+  if (!userId) {
+    redirect("/login");
+  }
   return (
     <div>
-      <Button>Hello World</Button>
+      <UserButton showName />
     </div>
   );
 }
