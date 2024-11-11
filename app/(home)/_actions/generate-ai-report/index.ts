9f8434e41221178c1a71eeb8ec0fbbdf234b76c6
Joclelson Rodrigues <jocleelson.159@hotmail.com>
2024-11-11 11:56:59 -0300

    feat: :sparkles: add ai report generate

---
+++ app/(home)/_actions/generate-ai-report/index.ts
@@ -0,0 +1,54 @@
+"use server";
+
+import { db } from "@/app/lib/prisma";
+import { auth, clerkClient } from "@clerk/nextjs/server";
+import { GenerateAiReportSchema, generateAiReportSchema } from "./schema";
+import OpenAi from "openai";
+
+const generateAiReport = async ({ month }: GenerateAiReportSchema) => {
+  generateAiReportSchema.parse({ month });
+
+  const { userId } = await auth();
+  if (!userId) {
+    throw new Error("Unauthorized");
+  }
+  const user = await clerkClient().users.getUser(userId);
+  const hasPremiumPlan = user.publicMetadata.subsscriptionPlan === "premium";
+  if (!hasPremiumPlan) {
+    throw new Error("User need a premium plan to generate a report");
+  }
+
+  const openai = new OpenAi({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+  const transactions = await db.transaction.findMany({
+    where: {
+      date: {
+        gte: new Date(`2024-${month}-01`),
+        lte: new Date(`2024-${month}-31`),
+      },
+    },
+  });
+  const messages = `Gere um relatório com insights sobre as minha finanças, com dicas e orientações de como melhorar minha vida financeira.
+    As transações estão divididas por ponto e virgula. A estrutura de cada uma é {DATA}-{TIPO}-{VALOR}-{CATEGORIA}. São elas:
+    ${transactions.map((transaction) => `${transaction.date.toLocaleDateString("pt-BR")}-${transaction.type}-${transaction.amount}-${transaction.category}`).join(";")}`;
+
+  const completion = await openai.chat.completions.create({
+    model: "gpt-4o-mini",
+    messages: [
+      {
+        role: "system",
+        content:
+          "Você é um especialista em gestão e organização de finanças pessoais. Você ajuda as pessoas a organizarem melhor suas finanças.",
+      },
+      {
+        role: "user",
+        content: messages,
+      },
+    ],
+  });
+
+  return completion.choices[0].message.content;
+};
+
+export default generateAiReport;
---
+++ app/(home)/_components/ai-report-button.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import { Button } from "@/app/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/app/components/ui/dialog";
+import { BotIcon, Loader2Icon } from "lucide-react";
+import generateAiReport from "../_actions/generate-ai-report";
+import { useState } from "react";
+import { ScrollArea } from "@/app/components/ui/scroll-area";
+import Markdown from "react-markdown";
+
+interface AiReportButtonProps {
+  month: string;
+}
+
+const AiReportButton = ({ month }: AiReportButtonProps) => {
+  const [report, serReport] = useState<string | null>();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleGenerateReport = async () => {
+    try {
+      setIsLoading(true);
+      const aiReport = await generateAiReport({ month });
+      serReport(aiReport);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <Dialog
+      onOpenChange={(open) => {
+        if (!open) {
+          serReport(null);
+        }
+      }}
+    >
+      <DialogTrigger asChild>
+        <Button variant="ghost">
+          Relatório IA
+          <BotIcon />
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="max-w-[600px]">
+        <DialogHeader>
+          <DialogTitle>Relatório IA</DialogTitle>
+          <DialogDescription>
+            Use inteligência artificial para gerar um relatório com insights
+            sobre suas finanças.
+          </DialogDescription>
+        </DialogHeader>
+        <ScrollArea className="prose max-h-[450px] prose-h3:text-white prose-h4:text-white prose-strong:text-white">
+          <Markdown>{report}</Markdown>
+        </ScrollArea>
+        <DialogFooter>
+          <DialogClose>
+            <Button variant="ghost">Fechar</Button>
+          </DialogClose>
+          <Button onClick={handleGenerateReport} disabled={isLoading}>
+            {isLoading && <Loader2Icon className="animate-spin" />}
+            Gerar Relatório
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default AiReportButton;
