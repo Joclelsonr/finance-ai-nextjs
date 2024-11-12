fca47a9a5347717013af1a6ebc400f06e7ed01dc
Joclelson Rodrigues <jocleelson.159@hotmail.com>
2024-11-12 16:31:19 -0300

    fix: :bug: bug in pie chart percentage

--- app/(home)/_components/percentage-item.tsx
+++ app/(home)/_components/percentage-item.tsx
@@ -13,7 +13,7 @@ const PercentageItem = ({ title, percentage, icon }: PercentageItemProps) => {
         <div className="rounded-lg bg-white bg-opacity-[4%] p-2">{icon}</div>
         <p className="text-sm text-muted-foreground">{title}</p>
       </div>
-      <p className="text-sm font-bold">{percentage}%</p>
+      <p className="text-sm font-bold">{percentage ? percentage : "0"}%</p>
     </div>
   );
 };
